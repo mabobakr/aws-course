b112d3dff18db09d1a25313d78eb675be4344902
mabobakr <m.abobakr10@yahoo.com>
2022-10-05 23:20:19 +0200

    update links

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram.us-east-1.elasticbeanstalk.com',
+  apiHost: 'http://udagram.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
